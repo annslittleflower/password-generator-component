9182000361325b0f82fb5ea83c831ed3db3e4470
Alex Bashlii <oleksandr@bstock.com>
2023-09-18 20:03:52 +0300

    public url fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(() => {
 	const config = {
 		plugins: [react()],
-		base: '/',
+		base: '/password-generator-component',
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src'),
